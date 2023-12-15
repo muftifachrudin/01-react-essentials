98c924729e0203d82328e7648ccb208885a5b8f2
MF <muftiarifachrudin@gmail.com>
2023-12-15 14:30:12 +0700

    split components

--- 01-react-essentials/src/components/CoreConcepts.jsx
+++ 01-react-essentials/src/components/CoreConcepts.jsx
@@ -1,9 +1,16 @@
-export default function CoreConcepts({ image, title, description }) {
+import CoreConcept from "./CoreConcept.jsx";
+import { CORE_CONCEPTS } from "../data.js";
+
+
+export default function CoreConcepts() {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+      </ul>
+    </section>
   );
 }
