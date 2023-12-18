cc328d13cdfe763adf198cdfc5a5e44a9eb1262b
MF <muftiarifachrudin@gmail.com>
2023-12-18 10:32:48 +0700

    create player components

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,10 +1,16 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player></Player>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
